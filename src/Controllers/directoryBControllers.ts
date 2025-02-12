a3377c3c27fe15c68c5e2edd5407869c7764f8a1
jhonjurado <jhonedisonjurado57@gmail.com>
2025-02-12 09:50:32 -0500

    Se actualiza la creacion de carpetas

--- src/Controllers/directoryBControllers.ts
+++ src/Controllers/directoryBControllers.ts
@@ -2,10 +2,12 @@ import { Response, Request } from 'express';
 import fs from 'fs/promises';
 import path from 'path';
 
+
 const createDirectory = async (req: Request, res: Response) => {
     try {
         const {nameDir} = req.body
         const basePath = 'src/Controllers/CarpetasBackups' 
+        
         const dirPath = path.join(basePath, nameDir);
         if (!nameDir) {
             return res.status(400).json({ message: 'El nombre de la carpeta es obligatorio' });
@@ -16,6 +18,14 @@ const createDirectory = async (req: Request, res: Response) => {
         res.status(500).json({ message: 'No se pudo crear el directorio', error: error});
     }
 };
+
+const ReadFiles = async (req:Request,res:Response) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
 
 export default {
     createDirectory
